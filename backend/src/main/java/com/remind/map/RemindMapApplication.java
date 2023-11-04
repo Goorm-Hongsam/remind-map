836b960e330785c8def0addcc18ec342cf989561
seoyeoning <minalunpark@naver.com>
2023-11-04 21:58:33 +0900

    CHORE: 웹훅 테스트

--- backend/src/main/java/com/remind/map/RemindMapApplication.java
+++ backend/src/main/java/com/remind/map/RemindMapApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RemindMapApplication {
-
+    // 테스트
     public static void main(String[] args) {
         SpringApplication.run(RemindMapApplication.class, args);
     }
