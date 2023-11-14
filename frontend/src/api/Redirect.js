ca36e32c5f76ddb7369cefb03dbe0749bd11d386
seoyeoning <minalunpark@naver.com>
2023-11-14 13:33:15 +0900

    FEAT: 로그인 테스트 코드

--- frontend/src/api/Redirect.js
+++ frontend/src/api/Redirect.js
@@ -7,19 +7,40 @@ const Redirect = () => {
   console.log(code);
   const navigate = useNavigate();
 
-  /*
   useEffect(() => {
     console.log(process.env.REACT_APP_URL);
-    axios.post(`http://localhost/kakao/kakaoLogin${code}`).then(r => {
-      console.log(r.data);
-
-      localStorage.setItem('name', r.data.user_name);
-
-      navigate('/');
+    axios.post(`http://localhost:8080/kakao/kakaoLogin/${code}`).then(r => {
+      console.log(r.headers['authorization']);
+      localStorage.setItem('Authorization', r.headers['authorization']);
+      // navigate('/');
     });
-  }, []);*/
+  }, []);
 
-  return <div>로그인 중입니다.</div>;
+  const handleTestClick = () => {
+    const jwtToken = localStorage.getItem('Authorization'); // localStorage에서 JWT 토큰을 가져옴
+    if (jwtToken) {
+      axios
+        .post(
+          'http://localhost:8080/test',
+          {},
+          {
+            headers: {
+              Authorization: jwtToken, // JWT 토큰을 헤더에 추가
+            },
+          },
+        )
+        .then(response => {
+          console.log('Response:', response.data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    } else {
+      console.log('No JWT token found');
+    }
+  };
+
+  return <button onClick={handleTestClick}>테스트</button>;
 };
 
 export default Redirect;
